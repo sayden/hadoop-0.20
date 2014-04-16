1727cb1ea2990a5e8e97dd85b4267a20ce63c898
Mario Castro <mariocaster@gmail.com>
2014-04-16 16:08:59 +0200

    Clear script inside timer

--- timer.sh
+++ timer.sh
@@ -18,9 +18,23 @@ if [ $# -eq 0 ]; then
 	echo '		lsm: aka "bin/hadoop dfsadmin -safemode leave"'
 	echo '		wordcount: aka "bin/hadoop jar hadoop-examples-0.20.205.0.jar wordcount /rime/pg4300.txt /rime/output"'
 	echo '		wc [arg1] [arg2] [arg3]: aka "bin/hadoop jar hadoop-examples-0.20.205.0.jar [arg1] [arg2] [arg3]"'
+	echo '		cl aka rm -rf logs/*'
+	echo '		ch aka rm -rf hdfs/*'
+	echo '		cf [arg] aka rm -rf [arg]'
 
 else 
 	case $1 in
+		cl) rm -rf logs/*
+			;;
+		ch) rm -rf hdfs/*
+			;;
+		cf) if [ $# eq 2 ]; then
+			rm -rf $2
+		    else
+			echo 'Not enough arguments: Usage'
+			echo '		timer cf [folder-to-delete]'
+		    fi
+			;;
 		sa) sbin/start-all.sh
 			;;
 		ka) sbin/stop-all.sh
