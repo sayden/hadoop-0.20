8e44cb55de1e795934556b70716b35a6817ba07b
Mario Castro <mariocaster@gmail.com>
2014-04-17 13:53:22 +0200

    Provisioning of hadoop through puppet

---
+++ more-conf-files/puppet/site.pp
@@ -0,0 +1,24 @@
+# Install git
+package {'git': ensure => installed }
+
+# Clone hadoop repository
+exec {'clone-repository': 
+	command => '/usr/bin/git clone https://github.com/sayden/hadoop-0.20.git /var/hadooptest',
+	require => Package['git']
+}
+
+# Open Firewall ports
+exec {'open-firewall':
+	command =>'/usr/bin/service firewalld stop'
+}
+
+# Create hdfs dir
+exec {'create-hdfs-dir':
+	command => '/usr/bin/mkdir /var/hadooptest/hdfs',
+	require => Exec['clone-repository']
+}
+
+# Update /etc/hosts
+exec {'update-hosts':
+	command => '/usr/bin/echo "192.168.1.107	asusnotebook" >> /etc/hosts'
+}
